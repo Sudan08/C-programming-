71e8347260f89ce6d61bb31ae011155b2f3f5913
Sudan Shakya <sudanshakya08@gmail.com>
2022-12-11 11:14:55 +0545

    added lab report

--- Week4/Lab report/ascending.c
+++ Week4/Lab report/ascending.c
@@ -1,10 +1,10 @@
 # include <stdio.h>
 void main(){
-	int arr[7] = {1,5,4,6,3,7,9};
-	int i , j , a,size = 7;
+	int arr[10] = {1,5,4,6,3,7,9,7,6,5};
+	int i , j , a,size = 10;
 	for (i=0;i<size;i++){
 		for(j=i+1;j<size;j++){
-			if(arr[i]>arr[j]){
+			if(arr[i]>=arr[j]){
 				a = arr[i];
 				arr[i] = arr[j];
 				arr[j] = a;
--- Week4/Lab report/sumcount.c
+++ Week4/Lab report/sumcount.c
@@ -4,12 +4,14 @@ void main(){
 	int i , sum = 0, arrlength;
 	int arr[9] = {1,2,3,4,5,6,7,8,9};
 	arrlength = sizeof(arr)/sizeof(arr[0]);
+	int count = 0 ;
 	
 	for (i=0;i<arrlength;i++){
 		if(arr[i] % 5 == 0 && arr[i]%2 != 0 && arr[i]%3 !=0){
 			sum = sum + arr[i];
+			count += 1;
 	}
 	}
-	printf("Count : %d",arrlength);
+	printf("Count : %d",count);
 	printf("\nsum is: %d",sum);
 }
